c5f670ff6d943729cfb5714157210f752b5332e7
Farid <farid.rahimoff@gmail.com>
2022-03-31 15:00:24 +0400

    final solutin of list comprehension

--- 8_list_comprehension.py
+++ 8_list_comprehension.py
@@ -9,5 +9,4 @@ if __name__ == '__main__':
         [i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k)!= n
         )
 
-    for item in permutations:
-        print(item)
+    print(permutations)
