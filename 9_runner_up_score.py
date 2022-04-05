d77fb597f599031e71325c9e3893fe31aca36a73
faridrh <farid.rahimov@yahoo.com>
2022-04-05 14:21:12 +0400

    cleaned version of running up score

--- 9_runner_up_score.py
+++ 9_runner_up_score.py
@@ -1,11 +1,11 @@
+# finding second maximum in an array
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    arr2 = list (arr)
-    arr2 = sorted(arr2)
+    arr2 = sorted (list (arr))
     max = arr2[-1]
     i = - 2
-    run_up_score = arr2[-2]
+    run_up_score = arr2[i]
     while run_up_score == max:
         i-=1
         run_up_score = arr2[i]
